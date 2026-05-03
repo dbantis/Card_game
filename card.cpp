ef3deac12796f3c22cb20fc499b29e916eef686d
Dimitris Bandis <dimosband@gmail.com>
2026-05-03 03:24:12 +0300

    Refactor Card class and restore suitToString function

--- card.cpp
+++ card.cpp
@@ -5,7 +5,7 @@ Card::Card(CardSuit s, int v, int p)
     points=p;
 }
 
-Card::~Card() { }
+Card::~Card() {}
 
 CardSuit Card::getSuit() const
 {
@@ -22,27 +22,6 @@ int Card::getPoints() const
     return points;
 }
 
-string suitToString(CardSuit suit)
-{
-    switch (suit)
-    {
-        case HEART:
-        return "Heart";
-       
-        case DIAMOND:
-        return "Diamond";
-      
-        case CLUB:
-        return "Club";
-        
-        case SPADE:
-        return "Spade";
-       
-        default:
-        return "Unknown";
-    }
-}
-
 Simple::Simple(CardSuit s, int v, int p):Card(s, v, p) {}
 
 CardEffect Simple::getEffect() const
@@ -57,50 +36,73 @@ bool Simple::isSpecial() const
 
 Seven::Seven(CardSuit s, int v, int p):Card(s, v , p) {}
 
-CardEffect Seven::getEffect()const
+CardEffect Seven::getEffect() const
 {
     return DRAW_TWO;
 }
 
-bool Seven::isSpecial()const
+bool Seven::isSpecial() const
 {
     return true;
 }
 
-Eight::Eight(CardSuit s, int v, int p):Card(s, v, p){}
+Eight::Eight(CardSuit s, int v, int p):Card(s, v, p) {}
 
 CardEffect Eight::getEffect() const
 {
     return PLAY_AGAIN;
 }
 
-bool Eight::isSpecial()const
+bool Eight::isSpecial() const
 {
     return true;
 }
 
 Nine::Nine(CardSuit s, int v, int p):Card(s, v, p) {}
 
-CardEffect Nine::getEffect()const
+CardEffect Nine::getEffect() const
 {
     return SKIP_NEXT;
 }
 
-bool Nine::isSpecial()const
+bool Nine::isSpecial() const
 {
     return true;
 }
 
-Ace::Ace(CardSuit s, int v, int p):Card(s, v, p){}
+Ace::Ace(CardSuit s, int v, int p):Card(s, v, p) {}
 
-CardEffect Ace::getEffect()const
+CardEffect Ace::getEffect() const
 {
     return CHANGE_SUIT;
 }
 
-bool Ace::isSpecial()const
+bool Ace::isSpecial() const
 {
     return true;
 }
+
+string suitToString(CardSuit suit)
+{
+    switch(suit)
+    {
+        case HEART:
+            return "Heart";
+       
+        case DIAMOND:
+            return "Diamond";
+      
+        case CLUB:
+            return "Club";
+        
+        case SPADE:
+            return "Spade";
+       
+        default:
+            return "Unknown";
+    }
+}
+
+
 
 
